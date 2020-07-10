d303a736b7d625e45f89b0e2e174a0e16543d8ef
jazeera <ckrishna@jazeeratech.in>
2020-07-10 23:00:15 +0530

    new tables

--- src/apex/AppExpress.ts
+++ src/apex/AppExpress.ts
@@ -3,10 +3,10 @@ import { json, urlencoded } from "body-parser";
 import { ProductControllers } from "../controllers/ProductControllers";
 import { CategoryControllers } from "../controllers/CategoriesController";
 import { SubCategoryControllers } from "../controllers/SubCategoriesController";
-// import { CitiesControllers } from "../controllers/CategoriesController";
-// import { SupplierControllers } from "../controllers/CategoriesController";
-// import { CustomerControllers } from "../controllers/CategoriesController";
-// import { OrdersControllers } from "../controllers/CategoriesController";
+import { CitiesControllers } from "../controllers/CitiesControllers";
+import { SupplierControllers } from "../controllers/SupplierControllers";
+import { CustomerControllers } from "../controllers/CustomerControllers";
+import { OrdersControllers } from "../controllers/OrdersControllers";
 
 export default class AppExpress {
   public express: any;
@@ -26,13 +26,12 @@ export default class AppExpress {
     });
     this.express.use("/", router);
 
-    let productController = new ProductControllers();
-    this.express.use("/api/products", productController.getRouter());
-
-    let categoriesController = new CategoryControllers();
-    this.express.use("/api/categories", categoriesController.getRouter())
-
-    let subCategoryControllers = new SubCategoryControllers();
-    this.express.use("/api/subcategories", subCategoryControllers.getRouter())
+    this.express.use("/api/products", new ProductControllers().getRouter())
+    this.express.use("/api/categories", new CategoryControllers().getRouter())
+    this.express.use("/api/subcategories", new SubCategoryControllers().getRouter())
+    this.express.use("/api/cities", new CitiesControllers().getRouter())
+    this.express.use("/api/suppliers", new SupplierControllers().getRouter())
+    this.express.use("/api/customers", new CustomerControllers().getRouter())
+    this.express.use("/api/orders", new OrdersControllers().getRouter())
   }
 }
--- src/entity/SubCategories.ts
+++ src/entity/SubCategories.ts
@@ -11,5 +11,5 @@ export class SubCategories {
 
   @JoinColumn({name:"category_id"})
   @OneToOne(type => Categories,category=>category.id)
-  category_id: Categories;
+  category_id: Categories["id"];
 }
