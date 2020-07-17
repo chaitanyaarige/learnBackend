ecdb312e641fb8fdfbf50e9452e640ec694740da
jazeera <ckrishna@jazeeratech.in>
2020-07-18 00:52:25 +0530

    sdfgh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jhgf: [],
+      users: {
+        full_name: '',
+        email: '',
+        phone: '',
+        address: '',
+        city: '',
+        street:'',
+        country: ''
+      }
+    }
+  }
+
+  async getData() {
+    await fetch('http://localhost:5200/api/customers/')
+      .then(response => response.json())
+      .then(response => {
+          this.setState({ jhgf: response.data })
+          console.log(this.state.jhgf)
+        })
+      .catch(error => console.log(error))
+  }
+
+  componentDidMount(){
+    this.getData();
+  }
+
+  render() {
+    return(
+      <div>
+        <ul>
+        {this.state.jhgf.map(hit =>
+          <li key={hit.objectID}>
+            {hit.city} ,  {hit.country}
+          </li>
+        )}
+      </ul>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
