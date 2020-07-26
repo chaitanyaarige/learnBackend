1dd1fa74f3a2355cbbe0f42aa779d365eab6352a
jazeera <ckrishna@jazeeratech.in>
2020-07-26 15:20:10 +0530

    react first commit

---
+++ frontends/src/routes/index.js
@@ -0,0 +1,12 @@
+// import Navheader from '../components/navBar/NavHeader.js'
+// import Footer from '../components/navBar/Footer.js'
+
+
+export default function Home() {
+  return (
+    <div className="container">
+      {/* <Navheader></Navheader> */}
+      {/* <Footer></Footer> */}
+    </div>
+  )
+}
