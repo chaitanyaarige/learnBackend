e732b95fc1ec56dec5fb80f90580a62a810524b0
chaitanya <ckrishna@jazeeratech.in>
2020-08-15 15:37:55 +0530

    status codes

--- src/controllers/AuthControllers.ts
+++ src/controllers/AuthControllers.ts
@@ -6,6 +6,7 @@ import bcrypt from "bcrypt";
 // import { Transport, createTransport } from "nodemailer"
 // import { hashSync, compareSync } from "bcrypt"
 
+
 export class AuthControllers {
   private componentName: string = "AuthControllers";
   private router: Router = Router();
@@ -15,21 +16,31 @@ export class AuthControllers {
   getRouter(): Router {
     this.router.post("/login", async (request: Request, response: Response) => {
       try {
-        let reqData: any
-        reqData = request.body ? request.body : {}
-        this.service.sessionInfo = request.body.sessionInfo
+        let reqData: any;
+        reqData = request.body ? request.body : {};
+        this.service.sessionInfo = request.body.sessionInfo;
 
-        // Check if username and password are sent
-        let { phone, password } = reqData
-        if (!(phone && password)) {
-          return response.status(401).send({error: 'Please enter phone and password'})
+        //Check if username and password are sent
+         let { phone, password } = reqData;
+         if (!(phone && password)) {
+           return response.status(401).send({error: 'Please enter phone and password'});
+         }
+
+        let existingCustomer = await this.customerService.findByPhone(reqData.phone);
+        if(!existingCustomer) {
+          return response.status(400).send({ data: 'Customer Not Exists'});
+        }  else {
+           const currentPassword = reqData.password;
+           const salt = bcrypt.genSaltSync(10);
+           const currentPasswordHash = bcrypt.hashSync(currentPassword, salt);
+           console.log(existingCustomer.password, currentPasswordHash)
+           if (!bcrypt.compareSync(existingCustomer.password, currentPasswordHash)) {
+             response.status(401).send({ status: 'The Current Password is Wrong' });
+           }
+          response.status(200).send({ existingCustomer });
         }
-
-        // let auth = await this.service.findOne(reqData);
-        // response.send({ status: 1, data: auth });
       } catch (error) {
-        console.log(error);
-        response.send({ status: 0, error: error });
+        response.status(500).send({error: 'Internal Server Error catch'});
       }
     });
 
